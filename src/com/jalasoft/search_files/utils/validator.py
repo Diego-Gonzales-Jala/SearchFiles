76c448b8752a4fbeeaea9194350669a0675dac28
Jose Vallejos <vallejosjhose@gmail.com>
2018-01-29 13:42:12 +0000

    no message

--- src/com/jalasoft/search_files/utils/validator.py
+++ src/com/jalasoft/search_files/utils/validator.py
@@ -3,20 +3,33 @@ import os
 
 class Validator:
     def __init__(self,  name_path = '', name_file = ''):
-        #self.name_dir = name_dir
         self.name_path = name_path
         self.name_file = name_file
+        self.no_valid_char_path = ['/', '*', '?', '"', '<','>','#']
 
-    def validator_absolute_path(self, absolute_path):
-        absolutePath = absolute_path
-        if absolutePath == os.path.join("name_dir", "name_path", "name_file"):
-            return True
+    def split_path(self,path):
+        return list(path)
+
+    def validator_exist_path(self, absolute_path):
+        if absolute_path != '':
+            if os.path.exists(absolute_path) == True:
+                return True
         else:
             False
 
-    def validator_non_allow_name_file_characters(self, nonCharactersValue):
-        while len(nonCharactersValue) == '/ \* ? " <>':
-            return " A file name can not contain any of those values:"
+    def validator_non_allow_name_file_characters(self, path):
+        lenght = len(path)
+        path_frag = self.split_path(path)
+
+        for i in range (lenght):
+            for j in range (len(self.no_valid_char_path)):
+                if self.no_valid_char_path[j] == path_frag[i]:
+                    value = False
+                    break
+                else:
+                    value = True
+
+        return value
 
     def validator_allowed_name_dir_characters(self, name_dir):
         while chr(name_dir) == "C" | "c" | "d" | "e" | "E" | "f" | "F" | "g" | "G":
@@ -35,10 +48,26 @@ class Validator:
         os.pathconf(path)
  """
 
-validatepat = ExpressValidator('C:\Python\Python36-32','84j')
-print (validatepat.validator_absolute_path('C:\Python\Python36-32'))
-print (validatepat.validator_non_allow_name_file_characters('C:\Python\Python36-32'))
-print (validatepat.validator_allowed_name_dir_characters('/jfkdf'))
-print (validatepat.validator_size_absolute_path('C:\Python\Python36-32'))
+validatepat = Validator()
+#print ("path ",validatepat.validator_exist_path('C:\Python\Python36-32'))
+print ("",validatepat.validator_non_allow_name_file_characters('C:\Python\Python#36-32\mono'))
+#print (validatepat.validator_allowed_name_dir_characters('/jfkdf'))
+#print (validatepat.validator_size_absolute_path('C:\Python\Python36-32'))
 
+
+
+"""
+f=open("archivo","r")
+it = (row for i,row in enumerate(r) if i>=0)
+for linea in it:
+  print linea
+
+
+def verify_ext(path, exten1)
+os.path.splitText(path)[1]
+if=exten1 == exten2:
+    return True
+else:
+    return false
+"""
 
